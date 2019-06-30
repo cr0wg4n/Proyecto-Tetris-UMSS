03e1c7ac0921b17548014a903712f9e3542fcc0f
cr0wg4n <mcm.crw@gmail.com>
2019-06-29 20:59:40 -0400

    array format

--- Tetris-Backend/App.py
+++ Tetris-Backend/App.py
@@ -5,10 +5,17 @@ App = TetrisApp()
 
 FPS = 1
 last_time = time.time()
+height = 16
+width = 10
 while True:
     lista = App.run("DOWN")
+    matrix = []
+    counter = 0
     for cy, row in enumerate(lista):
-        print(row)
+        if counter < 16:
+            matrix.append(row)
+        counter = counter + 1
+    print(matrix)
     new_time = time.time()
     sleep_time = ((1000.0 / FPS) - (new_time - last_time)) / 1000.0
     if sleep_time > 0:
